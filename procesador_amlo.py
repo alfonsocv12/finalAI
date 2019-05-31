edb7098dc87c63eedf0f04060f35bea098398062
alfonsocv12 <alfonsocvu@gmail.com>
2019-05-30 22:31:58 -0600

    Se hizo refactor de csv y leyo con python

--- procesador_amlo.py
+++ procesador_amlo.py
@@ -9,4 +9,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 
 # Read the spam csv
-# tweets_df =
+tweets_df = pd.read_csv('AMLO.csv',index_col=None, na_values=['NA'])
+tweets_df = tweets_df.drop(['id'],axis=1)
+
+#Part dedicated to show df
+# print(tweets_df.head(20))
