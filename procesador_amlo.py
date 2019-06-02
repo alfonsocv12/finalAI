f18f75638c22755a5fdb1548b2eab6a6c3136b80
alfonsocv12 <alfonsocvu@gmail.com>
2019-06-02 17:52:38 -0600

    Se agregaron procesamiento de predicciones

--- procesador_amlo.py
+++ procesador_amlo.py
@@ -6,7 +6,7 @@ from nltk.tokenize.toktok import ToktokTokenizer
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import MultinomialNB, GaussianNB, ComplementNB
 
 # Read the spam csv
 tweets_df = pd.read_csv('AMLO.csv',index_col=None, na_values=['NA'])
@@ -47,8 +47,8 @@ def preprocesing(log):
         if word in badwords:
             log_remove.append('MALAPALABRA')
         elif word not in stop_words:
-            # log_remove.append(stemmer.stem(word))
-            log_remove.append(word)
+            log_remove.append(stemmer.stem(word))
+            # log_remove.append(word)
     return log_remove
 
 tweets_df['log'] = tweets_df.apply(lambda tweets_df: nltk.word_tokenize(tweets_df['log'].lower()), axis=1)
@@ -72,7 +72,10 @@ X_train, X_test, Y_train, Y_test = train_test_split(data_features, data_labels, 
 '''
 Using MultinomialNB model
 '''
-clf = MultinomialNB()
+# clf = MultinomialNB()
+# clf.fit(X_train, Y_train)
+#
+clf = ComplementNB()
 clf.fit(X_train, Y_train)
 
 score = clf.score(X_test, Y_test)
@@ -82,3 +85,9 @@ print(score)
 Aqui es donde se procesaron los datos que tienen
 mas informacion
 '''
+# tweets_df_moreinfo = tweets_df_moreinfo.drop(['geo_coordinates','id_str','profile_image_url'], axis=1)
+# # print(tweets_df_moreinfo.head(20),'\n')
+# # print(tweets_df_moreinfo.describe(),'\n')
+# # print(tweets_df_moreinfo.apply(lambda x: x.isnull().any()),'\n')
+# # print(pd.DataFrame({'percent_missing': tweets_df_moreinfo.isnull().sum() * 100 / len(tweets_df_moreinfo)}),'\n')
+# print(pd.DataFrame({'percent_unique': tweets_df_moreinfo.apply(lambda log: log.unique().size/log.size*100)}),'\n')
