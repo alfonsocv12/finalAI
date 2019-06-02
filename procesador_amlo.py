abcb1aa8e2224617f98d341b9f30e62541c4b337
alfonsocv12 <alfonsocvu@gmail.com>
2019-06-02 12:40:23 -0600

    Se genero set de malas palabras

--- procesador_amlo.py
+++ procesador_amlo.py
@@ -12,5 +12,18 @@ from sklearn.naive_bayes import MultinomialNB
 tweets_df = pd.read_csv('AMLO.csv',index_col=None, na_values=['NA'])
 tweets_df = tweets_df.drop(['id'],axis=1)
 
+tweets_df_moreinfo = pd.read_csv('TAGS.csv', index_col=None, na_values=['NA'])
+
 #Part dedicated to show df
 # print(tweets_df.head(20))
+# print(tweets_df_moreinfo.head(20))
+
+'''
+Nltk tokenizacion, remover malas palabras y stop_words
+'''
+# token = ToktokTokenizer()
+stopwords = set(stopwords.words('spanish'))
+with open('badwords.txt', 'r') as file:
+    malas_palabras = (file.read().replace('\n', '')).split(',')
+badwords = set(malas_palabras)
+print(badwords)
