f5e8fe9fff9d7a0622f3dcb991180565c5c299a5
alfonsocv12 <alfonsocvu@gmail.com>
2019-06-02 13:48:56 -0600

    Se termino de preprocesar los datos

--- procesador_amlo.py
+++ procesador_amlo.py
@@ -19,11 +19,38 @@ tweets_df_moreinfo = pd.read_csv('TAGS.csv', index_col=None, na_values=['NA'])
 # print(tweets_df_moreinfo.head(20))
 
 '''
-Nltk tokenizacion, remover malas palabras y stop_words
+Aqui se carga nuestro txt con las
+malas palabras y se pone en un
+objeto set como las stopwords
 '''
-# token = ToktokTokenizer()
-stopwords = set(stopwords.words('spanish'))
+stop_words = set(stopwords.words('spanish'))
+
 with open('badwords.txt', 'r') as file:
-    malas_palabras = (file.read().replace('\n', '')).split(',')
+    malas_palabras = (file.read().replace('\n', '')).lower().split(',')
+    
 badwords = set(malas_palabras)
-print(badwords)
+
+'''
+Aqui se comienza a aplicar la tokenizacion
+remover stopwords y procesamiento de
+malas palabras
+'''
+
+def remove_badword_stopwords(log):
+    '''
+    Funcion encargada de remover
+    las malas palabras
+    '''
+    log_remove = []
+    for word in log:
+        if word in badwords:
+            log_remove.append('MALAPALABRA')
+        elif word not in stop_words:
+            log_remove.append(word)
+    return log_remove
+
+tweets_df['log'] = tweets_df.apply(lambda tweets_df: nltk.word_tokenize(tweets_df['log'].lower()), axis=1)
+tweets_df['log'] = tweets_df['log'].apply(lambda log: remove_badword_stopwords(log))
+
+print(tweets_df.head(20))
+print('')
